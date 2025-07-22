2f69f4e6bf1970050649f44235c1339823bd0a43
André Brasil <andremiguelbrasil@gmail.com>
2025-07-22 19:45:08 +0100

    components: style login button

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -3,5 +3,14 @@
 import { signIn } from "next-auth/react";
 
 export function Login() {
-  return <button onClick={() => signIn("github")}>Sign in with GitHub</button>;
+    return (
+        <>
+        <button 
+            className="text-neutral-950 hover:cursor-pointer p-2 bg-gray-600 rounded border-1 m-2 dark:bg-teal-950 text-neutral-300"
+            onClick={() => signIn("github")}
+        >
+              Sign in with GitHub
+          </button>
+      </>
+  );
 }
