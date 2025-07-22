17942250dca66bfa24b239da638af6b2c52d141c
André Brasil <andremiguelbrasil@gmail.com>
2025-07-22 18:02:46 +0100

    auth: setup buttons

---
+++ src/components/auth-buttons.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export function AuthButtons() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        <p>Signed in as {session.user?.email}</p>
+        <button class="border-black" onClick={() => signOut()}>
+            Sign out
+        </button>
+      </>
+    );
+  }
+
+  return <button onClick={() => signIn("github")}>Sign in with GitHub</button>;
+}
+
